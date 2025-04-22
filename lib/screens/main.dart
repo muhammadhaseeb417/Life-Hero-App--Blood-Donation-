7532605e2e6c9c114b38ae30de5d24f9b944a646
Muhammad Haseeb <bang526726@gmail.com>
2025-04-22 11:23:01 +0500

    configured firebase with my project

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:life_hero_app/utils/app_routes.dart';
 import 'package:life_hero_app/utils/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
